cc7c944ef138f9c8252744e8ef86276afbbd7e86
Marc Schmidt <m_schmidt94@web.de>
2021-10-06 18:48:54 +0200

    add carousel

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+import './bulma-carousel/js/bulma-carousel.js';
+// Initialize all elements with carousel class.
+const carousels = bulmaCarousel.attach('.carousel',{
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true
+});
