73e46ac9b7a2b4b5db53a3f2862a62caea0757c3
Marc Schmidt <m_schmidt94@web.de>
2021-10-11 13:40:24 +0200

    remove infinite scroll on story-slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,5 @@ import './bulma-carousel/js/bulma-carousel.js';
 // Initialize all elements with carousel class.
 const carousels = bulmaCarousel.attach('.carousel',{
     slidesToScroll: 1,
-    slidesToShow: 1,
-    infinite: true
+    slidesToShow: 1
 });
